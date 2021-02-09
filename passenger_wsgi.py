54d9c4884e2e2a707b6cc515b44b50ec080c1c74
Michael McNamara <theoregonconnection@gmail.com>
2021-02-08 17:57:21 -0800

    Update passenger_wsgi.py
    Second Test Update

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works UPDATED!\n'
+    message = 'It works UPDATED AGAIN!\n'
     version = 'Python %s\n' % sys.version.split()[0]
     response = '\n'.join([message, version])
     return [response.encode()]
